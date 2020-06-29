6a47806d20a2e8bf9e1512d96e6c8f3ecfb236de
Davidodari <davidkibzodari@gmail.com>
2020-06-30 01:27:09 +0300

    Initial commit

---
+++ app/src/main/java/com/github/odaridavid/composeapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.github.odaridavid.composeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.tooling.preview.Preview
+import com.github.odaridavid.composeapp.ui.ComposeAppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeAppTheme {
+        Greeting("Android")
+    }
+}
