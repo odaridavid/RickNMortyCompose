ec659f0eb26fb5571c8ba8c06ec3f94c3465513b
odaridavid <davidkibzodari@gmail.com>
2020-09-05 17:44:22 +0300

    remove IDE generated files

--- app/src/main/java/com/github/odaridavid/composeapp/MainActivity.kt
+++ app/src/main/java/com/github/odaridavid/composeapp/MainActivity.kt
@@ -3,31 +3,15 @@ package com.github.odaridavid.composeapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
-import com.github.odaridavid.composeapp.helpers.ComposeAppTheme
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAppTheme {
-                Greeting("Android")
-            }
+            Text(text = "Hello Compose")
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    ComposeAppTheme {
-        Greeting("Android")
-    }
 }
