d4e6962a08418b2b1fb6a9d8399da33eb46e7d89
odaridavid <davidkibzodari@gmail.com>
2020-09-06 00:06:56 +0300

    build compose tutorial

--- app/src/main/java/com/github/odaridavid/composeapp/MainActivity.kt
+++ app/src/main/java/com/github/odaridavid/composeapp/MainActivity.kt
@@ -3,15 +3,20 @@ package com.github.odaridavid.composeapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello Compose")
+            NewsStory()
         }
     }
 
 }
+
+
+
