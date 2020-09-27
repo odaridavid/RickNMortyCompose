8f0aefbd50e38f145868df5fb16ecfe412978761
odaridavid <davidkibzodari@gmail.com>
2020-09-27 18:13:12 +0300

    Migrate to kts

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,9 @@
+object Config {
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.2"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val applicationId = "com.github.odaridavid.ricknmorty"
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,7 @@
+object Plugins {
+    const val androidApplication = "com.android.application"
+    const val kotlinAndroid = "kotlin-android"
+    const val kotlinKapt = "kotlin-kapt"
+    const val apollo = "com.apollographql.apollo"
+    const val hilt = "dagger.hilt.android.plugin"
+}
